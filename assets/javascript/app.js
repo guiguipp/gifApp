9df60c4c4ac2b0287f5f365bccc3f4f52c7ae0cf
Guillaume Paugam <guipaugam@icloud.com>
2018-03-25 22:57:46 -0500

    Changed even listener. Gif added button. Display still first but can change state with click

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,18 +3,15 @@ $(document).ready(function() {
     var apiUrl = "https://api.giphy.com/v1/gifs/search?q="
     var apiKey = "&api_key=dc6zaTOxFJmzC"
     var apiLimitVerb = "&limit="
-    var apiLimitNum = 5;
+    var apiLimitNum = 10;
     var keyWord;
     var queryUrl;
     var embedCode;
     var gifsArray = ["Cat", "Dog","Hamster"]
-
     renderButtons();
 
-    $(".gif_btn").on("click", function() {
-        // animate();
+    $(document).on("click", ".gif_btn", function (){
         keyWord=($(this).attr("data-name"))
-        
         queryUrl= apiUrl+keyWord+apiKey+apiLimitVerb+apiLimitNum;
         console.log("queryUrl: " + queryUrl);
 
@@ -29,58 +26,54 @@ $(document).ready(function() {
                     var gifDiv = $("<div class='item'>");
                     var rating = results[i].rating;
                     var p = $("<p>").text("Rating: " + rating);
-                    var personImage = $("<img>");
-                    personImage.attr("src", results[i].images.fixed_height.url);
+                    var gifGenerated = $("<img>");
+                    gifGenerated.attr("src", results[i].images.fixed_height_still.url);
+                    // store the two attributes (still and animate) in the object itself, to retrieve them later
+                    gifGenerated.attr("data-still", results[i].images.fixed_height_still.url);
+                    gifGenerated.attr("data-animate", results[i].images.fixed_height.url);
+                    gifGenerated.attr("data-state","still");
                     gifDiv.append(p);
-                    gifDiv.append(personImage);
+                    gifDiv.append(gifGenerated);
                     $("#gif-view").append(gifDiv);
                 }
             }
+            return results;
         });
     });
 
     
-    function renderButtons() {
+function renderButtons() {
         $("#buttons-view").empty();
-        for (var i = 0; i < gifsArray.length; i++) {
-          var a = $("<button>");
-          a.addClass("gif_btn");
-          a.attr("data-name", gifsArray[i]);
-          a.text(gifsArray[i]);
-          $("#buttons-view").append(a);
+        for (var i = 0; i < gifsArray.length; i++) {            
+        var a = $("<button>");
+        a.addClass("gif_btn");
+        a.attr("data-name", gifsArray[i]);
+        a.text(gifsArray[i]);
+        $("#buttons-view").append(a);
         }
     }
 
-      
-
-    
-
-    //   $("#add-gif").on("click", function(event) {
-    //     event.preventDefault();
-    //     var newGif = $("#gif-input").val().trim();
-    //     gifsArray.push(newGif);
-    //     renderButtons();
-    //   });
-
-    
 
 
-
-
-
-        // function displayGifInfo() {
-  
-        //   var gif = $(this).attr("data-name");
-          
-  
-    //     function animate(){
-    //         var state = $(this).attr("data-state");
-    //         if (state === "still") {
-    //         $(this).attr("src", $(this).attr("data-animate"));
-    //         $(this).attr("data-state", "animate");
-    //         } else {
-    //         $(this).attr("src", $(this).attr("data-still"));
-    //         $(this).attr("data-state", "still");
-    //         }
-    //     };
+// creates new button with user input
+$("#add-gif").on("click", function(event) {
+    event.preventDefault();
+    var nameNewGif = $("#gif-input").val().trim();
+    gifsArray.push(nameNewGif);
+    renderButtons();
     });
+    
+
+// event listener for click on gif
+$(document).on("click", "img", function (){
+    console.log("Click recorded")
+    var state = $(this).attr("data-state");
+    if (state === "still") {
+        $(this).attr("src", $(this).attr("data-animate"));
+        $(this).attr("data-state", "animate");
+    } else {
+        $(this).attr("src", $(this).attr("data-still"));
+        $(this).attr("data-state", "still");
+    }
+    });
+});
